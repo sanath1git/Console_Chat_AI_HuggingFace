1d2554f28a2a77e8516916f1708b339568b0fbb7
sanath1git <115445072+sanath1git@users.noreply.github.com>
2025-11-14 07:24:59 +0530

    Update chatbot_api.py

--- chatbot_api.py
+++ chatbot_api.py
@@ -3,7 +3,7 @@ import requests
 API_URL = "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct"   
 
 headers = {
-    "Authorization": "Bearer -HuggingFace Access Tocken-"   # add your acess Tocken
+    "Authorization": "Bearer -HuggingFace Access Tocken-"   
 }
 
 def query(question):
@@ -28,14 +28,3 @@ while True:
         break
     response = query(user_input)
     print("Bot:", response)
-
-
-# We are importing the requests library to send HTTP requests to Hugging Face's API.
-# It helps us communicate with the cloud model just like a browser or app does.
-# Falcon-7B-Instruct mode
-# The headers variable is a dictionary in Python. (key-value pairs)
-# Bearer is a keyword that tells the server you're using token-based authentication.
-# It sends the prompt as a JSON payload to the API using POST.
-# 200 is the HTTP status code that means "OK" or "Success"
-# This takes the raw JSON reply from the API and converts it into a Python object (usually a list or dictionary).
-# We’re accessing the first item in the list
